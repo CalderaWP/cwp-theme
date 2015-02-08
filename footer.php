d1982089c65bfb4e6e51e8f4048cf31e4726cb19
Josh Pollock <jpollock412@gmail.com>
2015-02-08 15:20:22 -0500

    A little more adaptation for plugin page

--- footer.php
+++ footer.php
@@ -1,55 +1,28 @@
-	<div class="footer section bg-dark">
-		<div class="footer-inner section-inner">
-			<?php
-			$defaults = array(
-				'menu'            => 'Footer',
-				'container'       => 'div',
-				'container_class' => '',
-				'container_id'    => 'footer-menu-wrap',
-				'menu_class'      => 'menu',
-				'menu_id'         => 'footer-menu',
-				'echo'            => true,
-				'before'          => '',
-				'after'           => '',
-				'link_before'     => '',
-				'link_after'      => '',
-				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+<?php
+/**
+ * Footer Template
+ *
+ * @package   @cwp-theme
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2015 Josh Pollock
+ */
 
-			);
+$data = cwp_theme_plugin_data();
+?>
 
-			wp_nav_menu( $defaults );
-			?>
+<section id="footer">
+	<div class="container">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="nav">
+				<?php echo $data->menu( 'Footer' ); ?>
+			</div>
 		</div>
-
-		<div class="credits section bg-dark no-padding">
-
-			<div class="credits-inner section-inner">
-
-				<p class="credits-left">
-
-					<span>
-						Site &copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="CalderaWP LLC">CalderaWP LLC</a>
-					</span>
-					<span> &mdash; <a href="https://github.com/CalderaWP/cwp-com/" title="Source Code For This Site">Source</a></span>
-				</p>
-
-				<p class="credits-right">
-
-					<span>
-						<?php printf( __( 'Theme by <a href="%s">Anders Noren</a>', 'hemingway'), 'http://www.andersnoren.se' ); ?>
-					</span> &mdash;
-					<span>
-						<?php printf( __( 'Child Theme by <a href="%s">Josh Pollock</a>', 'cwp-com'), 'http://www.joshpress.net' ); ?>
-					</span> &mdash;
-					<a title="<?php _e('To the top', 'hemingway'); ?>" class="tothetop"><?php _e('Up', 'hemingway' ); ?> &uarr;</a>
-				</p>
-
-				<div class="clear"></div>
-
-			</div> <!-- /credits-inner -->
-
-		</div> <!-- /credits --></>
+		&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
 	</div>
+</section>
+
 
 </div> <!-- /big-wrapper -->
 
--- plugins-page.php
+++ plugins-page.php
@@ -22,23 +22,6 @@ echo $data->page();
 		</div>
 	</div>
 </section>
-<section id="footer">
-	<div class="container">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<div class="nav">
-				<?php echo $data->menu( 'Footer' ); ?>
-			</div>
-		</div>
-		&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
-	</div>
-</section>
 
-
-</div> <!-- /big-wrapper -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
 
 
