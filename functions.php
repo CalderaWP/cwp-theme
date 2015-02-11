fd04fcdd9cefc055138f7e6fb0e07af7ae6da259
Josh Pollock <jpollock412@gmail.com>
2015-02-11 01:41:33 -0500

    Make Jetpack not require connection to WPORG

--- functions.php
+++ functions.php
@@ -524,3 +524,10 @@ add_action( 'wp_head', function(){
 </script>
 <?php
 });
+
+/**
+ * Make Jetpack not require connection to WPORG
+ */
+add_action( 'plugins_loaded', function() {
+   add_filter( 'jetpack_development_mode', '__return_true' );
+});
