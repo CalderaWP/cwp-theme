788c3b725d38c334723ffd502c5fe1b9c80f7325
Josh Pollock <jpollock412@gmail.com>
2015-02-13 00:03:01 -0500

    Complete front page layout

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @TODO What this does.
+ * Front Page Template
  *
- * @package   @TODO
+ * @package   cwp-theme
  * @author    Josh Pollock <Josh@JoshPress.net>
  * @license   GPL-2.0+
  * @link      
@@ -35,41 +35,58 @@ $atts = $data->header_atts;
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-wrap">
 				<h3>WordPress Shouldn't Be Hard</3>
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
 				<img src="http://cwp.dev/public/cwp-content/uploads/2015/01/answers-query.png" alt="" class="front-page-img" />
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 
 				<div class="txt">
+
 					<p>WordPress makes very complex tasks possible. We chose WordPress because it was supposed to make our lives easier. At CalderaWP we create elegant solutions to complex problems to help you make beautiful sites faster and better.</p>
+
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="txt">
 					<p>We create plugins that are easy to use and provide an intuitive user experience in the back-end and front-end.</p>
 				</div>
 			</div>
-		</div>
-		<div class="container feature-container">
-			<h3>Featured Plugins</h3>
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-lg hidden-md all-plugins">
+				<a href="#" class="button big-cta">See All Plugins</a>
+			</div>
+
+
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="featured">
+				<h3>Featured Plugins</h3>
 				<div class="block-grid-3">
 					<?php echo cep_render_easy_pod( 'featured_products' ); ?>
 				</div>
 
 			</div>
-		</div>
 
-		<div class="container">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-wrap">
-				<a href="#" class="button">See All Plugins</a>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hidden-sm hidden-xs all-plugins">
+				<a href="#" class="button big-cta">See All Plugins</a>
 			</div>
-		</div>
+
+		</div><!--#front-feature-plugins-->
 
 		<div class="container feature-container" id="front-feature-caldera-answers">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-wrap">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 title-wrap ">
 				<h3>Learn WordPress The Right Way</3>
 			</div>
 
-			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-md-push-8 col-lg-push-8">
+				<img src="http://cwp.dev/public/cwp-content/uploads/2015/02/CalderaWP_Caldera_Answers.png" alt="Caldera Answers Logo" class="front-page-img" />
+			</div>
+
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-md-pull-4 col-lg-pull-4">
 				<div class="txt">
 					<p><?php echo CWP_Theme_Caldera_Answers::parent_page_link(); ?> is a set of resources, and training tools for learning WordPress development the right way, by CalderaWP's Josh Pollock. Josh is contributor to the top WordPress news and tutorial sites, including <a href="http://jpwp.me/tutsplus">Tuts+</a>, <a href="http://torquemag.io/author/joshp/">Torque</a>, <a href="http://jpwp.me/wpmu">WPMUDEV</a>, <a href="http://jpwp.me/wpbeginner">WPBegginer</a>.</p>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="txt">
 					<p>
 						<?php
 							printf( '%1s teach WordPress development using a project-oriented approach designed to give beginner and intermediate WordPress developers practical training using establish standards, the best tools and best practices.',
@@ -80,15 +97,8 @@ $atts = $data->header_atts;
 						?>
 					</p>
 				</div>
-				
-			</div>
-
-			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center">
-				<img src="http://cwp.dev/public/cwp-content/uploads/2015/02/CalderaWP_Caldera_Answers.png" alt="Caldera Answers Logo" class="front-page-img" />
 			</div>
 		</div>
-
-
 	</section>
 <?php
 
