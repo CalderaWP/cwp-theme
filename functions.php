44472de2a52ec062ae9f929c6439d1bd77b0e0b8
Josh Pollock <jpollock412@gmail.com>
2015-02-19 21:40:34 -0500

    don't load theme JS that doesn't do anything.

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function cwp_theme_scripts_styles() {
 
    $postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
-   wp_enqueue_script( 'cwp_theme', get_stylesheet_directory_uri() . "/assets/js/cwp_theme{$postfix}.js", array(), CWP_THEME_VERSION, true );
+   //wp_enqueue_script( 'cwp_theme', get_stylesheet_directory_uri() . "/assets/js/cwp_theme{$postfix}.js", array(), CWP_THEME_VERSION, true );
 
    wp_localize_script( 'cwp_theme', 'cwp_theme', array( 'adminajax' => admin_url( 'admin-ajax.php') ) );
 
